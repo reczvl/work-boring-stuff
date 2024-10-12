e99b71efe966e708813c539d6c6b55aaa9e05d40
reczvl <reczulskibartosz04@gmail.com>
2024-10-12 16:41:55 +0200

    cos tma

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int fnumber = 5;
+        int snumber = 10;
+        int sum = fnumber + snumber;
+        int dz = snumber / fnumber;
+
+        System.out.println(number);
     }
 }
