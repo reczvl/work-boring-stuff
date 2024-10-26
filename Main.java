00b2aeeee0d5c82e722af45e421f5db6b20fba65
test <cyprian.mill@gmail.com>
2024-10-26 15:00:26 +0200

    nnn

--- Main.java
+++ Main.java
@@ -1,12 +1,49 @@
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-        int fnumber = 5;
-        int snumber = 10;
-        int sum = fnumber + snumber;
-        int dz = snumber / fnumber;
 
-        System.out.println(number);
+
+
+//                                       WYŚWIETLANIE LISTY LICZB
+//
+//        System.out.print("Podaj cyfre calkowita: ");
+//        Scanner scanner = new Scanner(System.in);
+//        try {
+//            int number = scanner.nextInt();
+//            if(number < 1){
+//                throw new InputMismatchException();
+//            }
+//        for(int ii = 1; ii<=number; ii++) {
+//            System.out.println();
+//            for (int i = 1; i <= number; i++) {
+//                if (i == number) {
+//                    System.out.print(i + "");
+//                } else {
+//                    System.out.print(i + " ,");
+//                }
+//            }
+//        }
+//        }
+//        catch(InputMismatchException e){
+//            System.out.println("Coś poszło nie tak. Sprawdź czy podałeś liczbę całkowitą");
+//        }
+//                                           SPRAWDZANIE LICZBY
+//
+//        final int CONDITION_NUMBER = 10;
+//        System.out.println("Podana liczba to: "+number);
+//
+//        if(number > CONDITION_NUMBER) {
+//            System.out.print("Podana liczba jest wieksza od " + CONDITION_NUMBER);
+//        }
+//        else if(number < CONDITION_NUMBER) {
+//            System.out.print("Podana liczba jest mniejsza od " + CONDITION_NUMBER);
+//        }
+//        else {
+//            System.out.println("Podana liczba jest rowna " + CONDITION_NUMBER);
+//        }
     }
 }
